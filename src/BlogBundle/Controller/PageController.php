846879c3b9c4468b25656e956e6e7844bffb561d
DMC90 <admin@symfblog.com>
2016-03-29 21:21:25 +0200

    Created Enquiry and EnquiryType class for handling of forms

--- src/BlogBundle/Controller/PageController.php
+++ src/BlogBundle/Controller/PageController.php
@@ -2,6 +2,9 @@
 
 namespace BlogBundle\Controller;
 
+use BlogBundle\Entity\Enquiry;
+use BlogBundle\Form\EnquiryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PageController extends Controller
@@ -14,5 +17,24 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('BlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        if($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()) {
+
+                return $this->redirect($this->generateUrl('blog_contact'));
+            }
+        }
+
+        return $this->render('BlogBundle:Page:contact.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }

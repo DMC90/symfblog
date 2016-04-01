086851ca641f40a1c06494875e02a4c87c95ef98
DMC90 <admin@symfblog.com>
2016-04-01 22:53:31 +0200

    Setup database with blog entity, show blog routing, datafixtures for blog

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -6,8 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BlogController extends Controller
 {
+    /**
+     * Renders index html
+     */
     public function indexAction($name)
     {
         return $this->render('BlogBundle:Default:index.html.twig', ['name' => $name]);
+    }
+
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        /* var @em Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $blog = $em->getRepository('BlogBundle:Blog')->find($id);
+
+        if(!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+
+
     }
 }
